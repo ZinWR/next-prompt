c4ce97c67ec628e15e084e33aa2392a752b20300
QuanN <jeanquan98@gmail.com>
2024-04-28 21:33:26 -0500

    initialized create-prompt

--- app/create-prompt/page.tsx
+++ app/create-prompt/page.tsx
@@ -1,12 +1,26 @@
-import { FC } from 'react';
+"use client";
+
+import { FC, useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+import Form from '@components/Form';
 
 interface pageProps {
   
 };
 
 const CreatePrompt: FC<pageProps> = ({}) => {
+  const [submitting, setSubmitting] = useState<boolean>(false);
+  const [post, setPost] = useState<{ prompt: string; tag: string; }>({
+    prompt: '',
+    tag: ''
+  });
+  
   return (
-    <div>CreatePrompt</div>
+    <Form 
+
+    />
   );
 };
 
