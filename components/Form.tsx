99f9eba2cfcbf5a2a03b50ad5bd58cb4dba36474
QuanN <jeanquan98@gmail.com>
2024-04-28 21:46:46 -0500

    added textarea for form

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,9 +1,10 @@
 import { FC } from 'react';
+import Link from 'next/link';
 
 interface FormProps {
   type: string;
   post: { prompt: string; tag: string; };
-  setPost: () => void;
+  setPost: (arg0: { prompt: string; tag: string; }) => void;
   submiting: boolean;
   handleSubmit: () => void;
 };
@@ -16,7 +17,45 @@ const Form: FC<FormProps> = ({
   handleSubmit
 }) => {
   return (
-    <div>Form</div>
+    <section className='w-full max-w-full flex-start flex-col'>
+      <h1 className='head_text text-left'>
+        <span className='blue_gradient'>{type} Post</span>
+      </h1>
+      <p className='desc text-left max-w-md'>
+        {type} and share amazing prompts with the world, and let your magination run wild with any AI-powered platform.
+      </p>
+
+      <form
+        onSubmit={handleSubmit}
+        className='mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism'
+      >
+        {/* Write Prompt */}
+        <label>
+          <span className='font-satoshit font-semibold text-bae text-gray-700'>Your AI Prompt</span>
+        </label>
+        <textarea 
+          value={post.prompt}
+          onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+          placeholder='Write your prompt here...'
+          required
+          className='form_textarea'
+        />
+        {/* Tag  */}
+        <label>
+          <span className='font-satoshit font-semibold text-bae text-gray-700'>
+            Tag 
+            <span className='font-normal'> (#product, #webdevelopment, #idea)</span>
+          </span>
+        </label>
+        <textarea 
+          value={post.prompt}
+          onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+          placeholder='Write your prompt here...'
+          required
+          className='form_textarea'
+        />
+      </form>
+    </section>
   );
 };
 
