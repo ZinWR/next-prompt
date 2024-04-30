90005c802f4404f72b1c6ee7e170635da229824a
QuanN <jeanquan98@gmail.com>
2024-04-29 21:30:37 -0500

    added Lambda function route for prompt

--- app/api/prompt/new/route.ts
+++ app/api/prompt/new/route.ts
@@ -1,10 +1,19 @@
+import { NextApiRequest, NextApiResponse } from 'next';
 import { connectToDB } from '@utils/database';
+import Prompt from '@models/prompt';
 
-export const POST = async (req,res) => {
-    const { userId, prompt, tag } = await req.json();
+export const POST = async (req: NextApiRequest) => {
+    const { userId, prompt, tag } = await req.body.json();
      try {
         await connectToDB();
+        const newPrompt = new Prompt({ 
+         creator: userId, 
+         prompt,
+         tag
+        })
+         await newPrompt.save();
+         return new Response(JSON.stringify(newPrompt), { status: 201 });
      } catch (error) {
-        
+        console.log(error);
      }
 };
