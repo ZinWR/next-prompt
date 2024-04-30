329b502752562e5abf23fe99017b0bb480cf6faa
QuanN <jeanquan98@gmail.com>
2024-04-29 22:28:49 -0500

    started working on PromptCard

--- components/PromptCard.tsx
+++ components/PromptCard.tsx
@@ -1,12 +1,32 @@
-import { FC } from 'react';
+"use client";
+
+import { FC, useState } from 'react';
+import Image from 'next/image';
+import { useSession } from 'next-auth/react';
+import { usePathname, useRouter } from 'next/navigation';
 
 interface PromptCardProps {
-  
+  post: any;
+  handleTagClick: (arg0: any) => void;
+  handleEdit: (arg0: any) => void;
+  handleDelete: (arg0: any) => void;
 };
 
-const PromptCard: FC<PromptCardProps> = ({}) => {
+const PromptCard: FC<PromptCardProps> = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   return (
-    <div>PromptCard</div>
+    <div className='prompt_card'>
+      <div className='flex justify-between items-start gap-5'>
+        <div>
+          <Image 
+            src={post.creator.image}
+            alt='user_image'
+            width={40}
+            height={40}
+            className='rounded-full object-contain'
+          />
+        </div>
+      </div>
+    </div>
   );
 };
 
